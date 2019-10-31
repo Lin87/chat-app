d796fbcbfd6b0408ee7b044b5f3ec864d7614509
Ethan Lin <ethan.lin@uwex.edu>
2019-10-31 12:18:52 -0500

    generate ChatMessage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatNavBarComponent
+    ChatNavBarComponent,
+    ChatMessageComponent
   ],
   imports: [
     BrowserModule,
