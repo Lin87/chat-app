6a6f66f6be6fcef59fda4b6f85765ba773942a2e
Ethan Lin <ethan.lin@uwex.edu>
2019-10-31 12:26:08 -0500

    generated ChatThread component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
 import { ChatMessageComponent } from './chat-message/chat-message.component';
 import { ChatPageComponent } from './chat-page/chat-page.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatNavBarComponent,
     ChatMessageComponent,
-    ChatPageComponent
+    ChatPageComponent,
+    ChatThreadComponent
   ],
   imports: [
     BrowserModule,
