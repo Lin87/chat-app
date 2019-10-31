d18493afdc22910a06bc52ede39d38b420f3647f
Ethan Lin <ethan.lin@uwex.edu>
2019-10-31 12:16:15 -0500

    generated ChatNavBar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatNavBarComponent
   ],
   imports: [
     BrowserModule,
