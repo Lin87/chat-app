63891a3b668d42f90df0ae0e4bc9555624b734ba
Ethan Lin <ethan.lin@uwex.edu>
2019-10-31 12:26:51 -0500

    copied ChatThread code from book

--- src/app/chat-thread/chat-thread.component.ts
+++ src/app/chat-thread/chat-thread.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
+import { Observable } from 'rxjs';
+import { ThreadsService } from './../thread/threads.service';
+import { Thread } from '../thread/thread.model';
 
 @Component({
   selector: 'chat-thread',
@@ -6,10 +15,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-thread.component.css']
 })
 export class ChatThreadComponent implements OnInit {
+  @Input() thread: Thread;
+  selected = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public threadsService: ThreadsService) {
   }
 
+  ngOnInit(): void {
+    this.threadsService.currentThread
+      .subscribe( (currentThread: Thread) => {
+        this.selected = currentThread &&
+          this.thread &&
+          (currentThread.id === this.thread.id);
+      });
+  }
+
+  clicked(event: any): void {
+    this.threadsService.setCurrentThread(this.thread);
+    event.preventDefault();
+  }
 }
