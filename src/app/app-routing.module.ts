ca7229dd0a23baa10a0f445486c0c04d9cacde37
Ethan Lin <ethan.lin@uwex.edu>
2019-10-31 14:50:24 -0500

    added page not found route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChatPageComponent } from './chat-page/chat-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
-  { path: '', component: ChatPageComponent }
+  { path: '', component: ChatPageComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
