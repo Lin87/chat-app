5ed62b4f6278664becbbc86df0ae6ef723207049
Ethan Lin <ethan.lin@uwex.edu>
2019-10-31 13:03:51 -0500

    Updated ThreadsService code to latest version of rxjs

--- src/app/thread/threads.service.ts
+++ src/app/thread/threads.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Subject, BehaviorSubject, Observable } from 'rxjs/Rx';
+import { Subject, BehaviorSubject, Observable } from 'rxjs';
+import { map, combineLatest } from 'rxjs/operators';
 import { Thread } from './thread.model';
 import { Message } from '../message/message.model';
 import { MessagesService } from '../message/messages.service';
@@ -24,45 +25,91 @@ export class ThreadsService {
 
   constructor(public messagesService: MessagesService) {
 
-    this.threads = messagesService.messages
-      .map( (messages: Message[]) => {
-        const threads: {[key: string]: Thread} = {};
-        // Store the message's thread in our accumulator `threads`
-        messages.map((message: Message) => {
-          threads[message.thread.id] = threads[message.thread.id] ||
-            message.thread;
+    // this.threads = messagesService.messages
+    //   .map( (messages: Message[]) => {
+    //     const threads: {[key: string]: Thread} = {};
+    //     // Store the message's thread in our accumulator `threads`
+    //     messages.map((message: Message) => {
+    //       threads[message.thread.id] = threads[message.thread.id] ||
+    //         message.thread;
 
-          // Cache the most recent message for each thread
-          const messagesThread: Thread = threads[message.thread.id];
-          if (!messagesThread.lastMessage ||
-              messagesThread.lastMessage.sentAt < message.sentAt) {
-            messagesThread.lastMessage = message;
+    //       // Cache the most recent message for each thread
+    //       const messagesThread: Thread = threads[message.thread.id];
+    //       if (!messagesThread.lastMessage ||
+    //           messagesThread.lastMessage.sentAt < message.sentAt) {
+    //         messagesThread.lastMessage = message;
+    //       }
+    //     });
+    //     return threads;
+    //   });
+
+      this.threads = messagesService.messages.pipe(
+        map(
+          (messages: Message[]) => {
+            const threads: {[key: string]: Thread} = {};
+            // Store the message's thread in our accumulator `threads`
+            messages.map((message: Message) => {
+              threads[message.thread.id] = threads[message.thread.id] ||
+                message.thread;
+    
+              // Cache the most recent message for each thread
+              const messagesThread: Thread = threads[message.thread.id];
+              if (!messagesThread.lastMessage ||
+                  messagesThread.lastMessage.sentAt < message.sentAt) {
+                messagesThread.lastMessage = message;
+              }
+            });
+            return threads;
           }
-        });
-        return threads;
-      });
+        )
+      );
 
-    this.orderedThreads = this.threads
-      .map((threadGroups: { [key: string]: Thread }) => {
+    // this.orderedThreads = this.threads
+    //   .map((threadGroups: { [key: string]: Thread }) => {
+    //     const threads: Thread[] = _.values(threadGroups);
+    //     return _.sortBy(threads, (t: Thread) => t.lastMessage.sentAt).reverse();
+    //   });
+
+    this.orderedThreads = this.threads.pipe(
+      map((threadGroups: { [key: string]: Thread }) => {
         const threads: Thread[] = _.values(threadGroups);
         return _.sortBy(threads, (t: Thread) => t.lastMessage.sentAt).reverse();
-      });
+      })
+    );
 
-    this.currentThreadMessages = this.currentThread
-      .combineLatest(messagesService.messages,
-                     (currentThread: Thread, messages: Message[]) => {
-        if (currentThread && messages.length > 0) {
+    // this.currentThreadMessages = this.currentThread
+    //   .combineLatest(messagesService.messages,
+    //                  (currentThread: Thread, messages: Message[]) => {
+    //     if (currentThread && messages.length > 0) {
+    //       return _.chain(messages)
+    //         .filter((message: Message) =>
+    //                 (message.thread.id === currentThread.id))
+    //         .map((message: Message) => {
+    //           message.isRead = true;
+    //           return message; })
+    //         .value();
+    //     } else {
+    //       return [];
+    //     }
+    //   });
+
+    this.currentThreadMessages = this.currentThread.pipe(
+      combineLatest(messagesService.messages,
+        (currentThread: Thread, messages: Message[]) => {
+          if (currentThread && messages.length > 0) {
           return _.chain(messages)
-            .filter((message: Message) =>
-                    (message.thread.id === currentThread.id))
-            .map((message: Message) => {
-              message.isRead = true;
-              return message; })
-            .value();
-        } else {
+          .filter((message: Message) =>
+                (message.thread.id === currentThread.id))
+          .map((message: Message) => {
+          message.isRead = true;
+          return message; })
+          .value();
+          } else {
           return [];
+          }
         }
-      });
+      )
+    );
 
     this.currentThread.subscribe(this.messagesService.markThreadAsRead);
   }
